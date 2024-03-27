f00161ee9c9c6c2cc3bd9a1d2cd4b1fd60b0fe4c
TetianaPerinha <68566253+TetianaPerinha@users.noreply.github.com>
2024-03-26 21:57:07 -0500

    Modify test for reset password functionality

--- tests/authentication/test_auth_change_password.py
+++ tests/authentication/test_auth_change_password.py
@@ -11,6 +11,7 @@ from configs import (
     email_address_to_connect2,
     gmail_password2,
 )
+from framework.asserts.common import assert_message_in_response
 from framework.endpoints.authenticate_api import AuthenticateAPI
 from framework.tools.generators import generate_password
 
@@ -65,4 +66,70 @@ class TestResetPassword:
         ):
             AuthenticateAPI().authentication(
                 email=email, password=new_password, expected_status_code=200
+            )
+
+    @pytest.mark.critical
+    @severity(severity_level="CRITICAL")
+    @title("Test change password through reset password")
+    @description(
+        "Give user registered."
+        "WHEN user sent request to change password."
+        "THEN status HTTP CODE = 200 "
+    )
+    @pytest.mark.parametrize(
+        "code_for_reset_password,email, new_password, expected_status_code, expected_message_part",
+        [
+            pytest.param(
+                " ", None, generate_password(8), 400, "Code is the mandatory attribute"
+            ),
+            pytest.param(
+                "testtest",
+                None,
+                generate_password(8),
+                400,
+                "Incorrect token format, token must be ###-###-###",
+            ),
+            pytest.param(
+                "123-123-123 ", None, generate_password(8), 400, "Incorrect token"
+            ),
+            pytest.param(
+                "12345678910 ",
+                None,
+                generate_password(8),
+                400,
+                "Incorrect token format, token must be ###-###-###",
+            ),
+            pytest.param(
+                "$*@!_+_*&%",
+                None,
+                generate_password(8),
+                400,
+                "Incorrect token format, token must be ###-###-###",
+            ),
+        ],
+    )
+    def test_reset_password_incorrect_code(
+        self,
+        create_authorized_user,
+        code_for_reset_password,
+        email,
+        new_password,
+        expected_status_code,
+        expected_message_part,
+    ):
+        with step("Registration user"):
+            user = create_authorized_user["user"]
+
+        with step("Sent request to reset password"):
+            email = user["email"] if email is None else email
+            response_reset_password = AuthenticateAPI().change_password_through_reset(
+                email=email,
+                code_for_reset_password=code_for_reset_password,
+                new_password=new_password,
+                expected_status_code=400,
+            )
+
+        with step("Verify error message"):
+            assert_message_in_response(
+                response=response_reset_password, expected_message=expected_message_part
             )
