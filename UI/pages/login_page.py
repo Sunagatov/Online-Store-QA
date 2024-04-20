64d50d708cd2edcecbb573f1a0d5a95a9440a48a
ilsinyakov <ilsinyakov@gmail.com>
2024-04-20 23:17:19 +0300

    test drop-down not present

--- UI/pages/login_page.py
+++ UI/pages/login_page.py
@@ -1,11 +1,15 @@
 from .base_page import BasePage
-from .locators import LoginPageLocators
+from .locators import BasePageLocators, LoginPageLocators
 
 
 class LoginPage(BasePage):
     def go_to_registration_page(self):
         link = self.browser.find_element(*LoginPageLocators.REGISTER_BUTTON)
         link.click()
+    
+    # check that sort drop-down is not present on the page    
+    def is_dropdown_present(self):
+        self.is_element_present(*BasePageLocators.SORT_DROPDOWN)
 
     def login_existing_user(self, email, password):
         email_field = self.browser.find_element(*LoginPageLocators.EMAIL_FIELD)
@@ -13,4 +17,4 @@ class LoginPage(BasePage):
         login_button = self.browser.find_element(*LoginPageLocators.LOGIN_BUTTON)
         email_field.send_keys(email)
         password_field.send_keys(password)
-        login_button.click()
+        login_button.click()    
