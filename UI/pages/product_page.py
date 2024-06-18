e3a6110eb6ce353afd11e0815df66ac9454fbd25
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-06-18 11:24:42 +0300

    fix4

--- UI/pages/product_page.py
+++ UI/pages/product_page.py
@@ -231,7 +231,7 @@ class ProductPage(BasePage):
     def is_submit_button_not_active(self):
         submit_review_button = self.browser.find_element(*ProductPageLocators.SUBMIT_REVIEW_BUTTON)
         print(submit_review_button.get_attribute('disabled'))
-        if submit_review_button.get_attribute('disabled') is None:
+        if submit_review_button.get_attribute('disabled') is not None:
             return True
         else: 
             return False
