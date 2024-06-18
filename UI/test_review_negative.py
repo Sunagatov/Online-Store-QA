a9b418dfe23a1c76a8b147244016001c1f688565
ilsinyakov <140279745+ilsinyakov@users.noreply.github.com>
2024-06-18 11:48:17 +0300

    empty

--- UI/test_review_negative.py
+++ UI/test_review_negative.py
@@ -28,6 +28,8 @@ class TestReviewNegative:
             product_page.set_rating()
             product_page.fill_review(parameterize_text_review_negative[0][0])
             assert product_page.is_submit_button_not_active(), "Submit review button is active"
+
+    @pytest.mark.xfail(reason="Requirements is not approved", run=True)
     def test_not_allowed_symbols_review(self, browser):
         with step('Login user'):
                 login_user(browser, link)
@@ -38,4 +40,16 @@ class TestReviewNegative:
             product_page.click_add_review()
             product_page.set_rating()
             product_page.fill_review(parameterize_text_review_negative[1][0])
+            assert product_page.is_submit_button_not_active(), "Submit review button is active"
+
+    def test_emppty_review(self, browser):
+        with step('Login user'):
+                login_user(browser, link)
+        with step('Delete old review'):
+            delete_old_review(browser, link)
+        with step('Add non latin letters review'):
+            product_page = ProductPage(browser, browser.current_url)
+            product_page.click_add_review()
+            product_page.set_rating()
+            product_page.fill_review(parameterize_text_review_negative[2][0])
             assert product_page.is_submit_button_not_active(), "Submit review button is active"
