003968fec08aa2a53a4f78c3893d44ec6474d2df
ilsinyakov <ilsinyakov@gmail.com>
2024-08-18 11:38:27 +0300

    add negative tests for main page

---
+++ UI/test_main_page_negative.py
@@ -0,0 +1,50 @@
+from allure import step, title, severity, story, severity_level
+import pytest
+import re
+# from time import sleep
+
+from .pages.BasePage import BasePage
+from .configs import link
+
+
+@story("Product Catalog")
+@title("Negative Test for Main Page")
+# @allure.description("")
+# @allure.tag("")
+@severity(severity_level.NORMAL)
+class TestMainPageNegative:
+    @title("Check filter products by 'price from' (digits with other symbols) on main page")
+    @pytest.mark.xfail(reason='Bug is not fixed', run=True)
+    @pytest.mark.parametrize("price", ('-4', '4-', 'a4', '4a', '4.4', '4,4', ' 4', '4 '))
+    def test_filter_products_by_price_from_numbers(self, browser, price):
+        main_page = BasePage(browser, link)
+        main_page.open()
+        price_from = price
+        price_to = '1000'
+
+        main_page.filter_products_by_price(price_from, price_to)
+
+        # remove all non-digit symbols from price
+        price_from_after_processing = re.sub(r'\D', '', price_from)
+
+        with step('Check that filtering by price is correct'):
+            assert main_page.is_filtering_by_price_correct(price_from_after_processing, price_to), \
+                'The filtering is not correct'
+
+    @title("Check filter products by 'price to' (digits with other symbols) on main page")
+    @pytest.mark.xfail(reason='Bug is not fixed', run=True)
+    @pytest.mark.parametrize("price", ('-4', '4-', 'a4', '4a', '4.4', '4,4', ' 4', '4 '))
+    def test_filter_products_by_price_to_numbers(self, browser, price):
+        main_page = BasePage(browser, link)
+        main_page.open()
+        price_from = '0'
+        price_to = price
+
+        main_page.filter_products_by_price(price_from, price_to)
+
+        # remove all non-digit symbols from price
+        price_to_after_processing = re.sub(r'\D', '', price_to)
+
+        with step('Check that filtering by price is correct'):
+            assert main_page.is_filtering_by_price_correct(price_from, price_to_after_processing), \
+                'The filtering is not correct'
